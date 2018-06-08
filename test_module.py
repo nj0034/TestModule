1bddfb8174b4798799316006a58c6aa52c0d3213
namjin lee <nj@devhi.me>
2018-06-08 17:48:20 +0900

    학부,소프트웨어 추가 예술 수정 필요

--- test_module.py
+++ test_module.py
@@ -6,9 +6,11 @@ import time
 import re
 import urllib.parse
 import json
+from elasticsearch import Elasticsearch
 
-json_data = open('json\일반.json', encoding='UTF8').read()
+json_data = open('json\소프트웨어.json', encoding='UTF8').read()
 XPATH_JSON = json.loads(json_data)
+es = Elasticsearch('search-toast-rgeq2lspq63rey535bxmff5m4y.ap-northeast-2.es.amazonaws.com')
 
 
 class TestModule(Sailer):
@@ -40,16 +42,21 @@ class TestModule(Sailer):
 
     def next_button(self):
         in_prop_list = self.props.keys()
-        top_article_url = self.xpath(XPATH_JSON['top_article_xpath']).get_attribute('href')
-        self.go(top_article_url)
+        # top_article_url = self.xpath(XPATH_JSON['top_article_xpath']).get_attribute('href')
+        # self.go(top_article_url)
+        self.wait_xpath(self.rule['top_article_xpath'])
+        # self.xpath(self.rule['top_article_xpath']).click()
+        self.go('http://cs.skku.edu/open/notice/view/2253')
 
         while (True):
             parsing_result_json_list = [{"url": self.current_url}]
             parsing_result_json_list = self.parsing_in_props(in_prop_list, parsing_result_json_list)
+            print(parsing_result_json_list)
 
             # es에 parsing_result_json_list 저장(top 글이면 저장 안함)
+            es.create(index="content", doc_type="_doc", id='test', body=parsing_result_json_list[0])
 
-            next_button_url = self.xpath(XPATH_JSON['next_button_xpath']).get_attribute('href')
+            next_button_url = self.xpath(self.rule['next_button_xpath']).get_attribute('href')
             if next_button_url:
                 self.go(next_button_url)
             else:
@@ -66,8 +73,8 @@ class TestModule(Sailer):
             else:
                 pass
 
-        title_url_xpaths = self.xpaths(self.props['title_url']['xpath'])
-        href_list = [title_url_xpath.get_attribute('href') for title_url_xpath in title_url_xpaths]
+        title_url_web_elements = self.xpaths(self.props['title_url']['xpath'])
+        href_list = [title_url_web_element.get_attribute('href') for title_url_web_element in title_url_web_elements]
 
         # 자바스크립트 url
         if self.props['title_url']['param_regex']:
@@ -82,17 +89,19 @@ class TestModule(Sailer):
         else:
             parsing_result_json_list = [{"url": href} for href in href_list]
 
+        # 리스트에서 파싱해야 하는 props
         for out_prop in out_prop_list:
-            xpath_list = self.xpaths(self.props[out_prop]['xpath'])
-            for xpath, parsing_result_json in zip(xpath_list, parsing_result_json_list):
-                out_prop_json = self.parsing_prop(out_prop)
+            web_elements = self.xpaths(self.props[out_prop]['xpath'])
+            for web_element, parsing_result_json in zip(web_elements, parsing_result_json_list):
+                out_prop_json = self.parsing_prop(prop=out_prop, web_element=web_element)
                 parsing_result_json.update(out_prop_json)
 
         parsing_result_json_list = self.parsing_in_props(in_prop_list, parsing_result_json_list)
 
         # es에 저장(top 글이면 저장 안함)
-
-        print(parsing_result_json_list)
+        for parsing_result_json in parsing_result_json_list:
+            es.index(index="content", doc_type="_doc", body=parsing_result_json, request_timeout=60)
+            print(parsing_result_json)
 
         time_interval = [int(n) for n in self.parser['interval'].split('-')]
         random_time = random.randint(*time_interval)
@@ -111,33 +120,45 @@ class TestModule(Sailer):
 
                     in_prop_json = self.parsing_regex_prop(in_prop, regex, html)
                 else:
-                    in_prop_json = self.parsing_prop(in_prop)
+                    in_prop_json = self.parsing_prop(prop=in_prop)
 
                 parsing_result_json.update(in_prop_json)
 
         return parsing_result_json_list
 
-    def parsing_prop(self, prop):
+    def parsing_prop(self, **kwargs):
+        prop = kwargs.get('prop', '')
         xpath = self.props[prop]['xpath']
+
+        web_element = kwargs.get('web_element', '')
+
+        if not web_element:
+            try:
+                web_element = self.xpath(xpath)
+            except:
+                web_element = None
+
+        web_elements = self.xpaths(xpath)
+
         prop_type = self.props[prop]['type']
 
         if prop_type == 'content':
             prop_json = {
-                "content_text": self.xpath(xpath).text,
-                "content_HTML": self.xpath(xpath).get_attribute('innerHTML'),
+                "content_text": web_element.text,
+                "content_HTML": web_element.get_attribute('innerHTML'),
             }
         elif prop_type == 'date':
-            date = self.xpath(xpath).text
+            date = web_element.text
             date = date.replace('오전', 'AM')
             date = date.replace('오후', 'PM')
 
             format = self.props[prop]['format']
             date = convert_datetime(date, format, '%Y-%m-%d %H:%M:%S')
             prop_json = {
-                "date": date
+                "created_datetime": date
             }
         elif prop_type == 'image':
-            img_url_list = [xpath.get_attribute('src') for xpath in self.xpaths(xpath) if xpath]
+            img_url_list = [web_element.get_attribute('src') for web_element in web_elements if web_element]
 
             # s3에 저장하는 함수 넣기 + s3 url list 만들기
             img_s3_url_list = list()
@@ -148,7 +169,8 @@ class TestModule(Sailer):
             }
         elif prop_type == 'file':
             attach_url_list = list()
-            href_list = [urllib.parse.unquote(xpath.get_attribute('href')) for xpath in self.xpaths(xpath) if xpath]
+            attach_name_list = list()
+            href_list = [urllib.parse.unquote(web_element.get_attribute('href')) for web_element in web_elements if web_element]
 
             if self.props[prop]['param_regex']:
                 param_list = [re.findall(self.props[prop]['param_regex'], href) for href in href_list]
@@ -163,9 +185,9 @@ class TestModule(Sailer):
             else:
                 attach_url_list = href_list
 
-            attach_name_list = re.findall(self.props[prop]['name_regex'],
-                                          self.xpath(self.props[prop]['HTML_xpath']).get_attribute('innerHTML'))
-
+            if attach_url_list:
+                attach_name_list = re.findall(self.props[prop]['name_regex'],
+                                              self.xpath(self.props[prop]['HTML_xpath']).get_attribute('innerHTML'))
 
             # s3에 저장하는 함수 넣기 + s3 url list 만들기
             attach_s3_url_list = list()
@@ -177,7 +199,7 @@ class TestModule(Sailer):
             }
         else:
             prop_json = {
-                prop: self.xpath(xpath).text
+                prop: web_element.text
             }
 
         print(prop_json)
